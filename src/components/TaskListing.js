bb72a8e96666b6fa6c117738e9d29b847c0f804c
Govinda <fearlessjack00@gmail.com>
2023-12-18 13:15:45 +0530

    Update devDependencies with react-scripts version

--- src/components/TaskListing.js
+++ src/components/TaskListing.js
@@ -5,7 +5,7 @@ const TaskListing = ({ tasks, setTasks, onNewTask, onTaskCreated, onTaskDeleted,
   console.log('Rendering TaskListing component');
 
   useEffect(() => {
-  fetch('https://main--helpful-mermaid-bd0430.netlify.app/tasks')
+  fetch('https://main--helpful-mermaid-bd0430.netlify.app/')
       .then((response) => {
         if (!response.ok) {
           throw new Error('Network response was not ok');
